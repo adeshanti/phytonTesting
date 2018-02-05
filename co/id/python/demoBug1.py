91ef0b483a620b9f8fd74bc2c45ef694618d152a
adeshanti <36162743+adeshanti@users.noreply.github.com>
2018-02-05 23:24:32 +0800

    update Source
    add try to sign out and login again

--- co/id/python/demoBug1.py
+++ co/id/python/demoBug1.py
@@ -4,7 +4,7 @@ Cart is empty after logout
 @author: deShantiLa
 '''
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import time
 
 user = "made.shanti@mail.com"
 pwd = "123456A"
@@ -23,4 +23,20 @@ driver.get("http://automationpractice.com/index.php?id_category=3&controller=cat
 driver.get("http://automationpractice.com/index.php?id_product=1&controller=product")
 
 elem = driver.find_element_by_id("add_to_cart").click()
+time.sleep(10)
+
+driver.get("http://automationpractice.com/index.php?id_product=1&controller=product")
+
+driver.get("http://automationpractice.com/index.php?mylogout=")
+
+time.sleep(5)
+
+driver.get("http://automationpractice.com/index.php?controller=my-account")
+
+elem = driver.find_element_by_id("email")
+elem.send_keys(user)
+elem = driver.find_element_by_id("passwd")
+elem.send_keys(pwd)
+elem = driver.find_element_by_id("SubmitLogin").click()
+
 
